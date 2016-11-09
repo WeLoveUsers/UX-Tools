939eb70de41c9f1adb28f85d43521adbb3941a0e
Florent Jaouali <florent.jaouali@gmail.com>
2016-11-09 15:31:12 +0100

    Re-enable subscription

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable, :registerable
   devise :database_authenticatable,
+         :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :projects, :dependent => :delete_all
